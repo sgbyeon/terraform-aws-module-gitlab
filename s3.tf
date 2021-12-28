3168b95d66545602a216bd7b7bfcfb3047f416c7
sgbyeon <sg.byeon1@gmail.com>
2021-12-28 15:47:26 +0900

    [dev] updates

--- s3.tf
+++ s3.tf
@@ -1,13 +1,13 @@
 resource "aws_s3_bucket" "this" {
   for_each = toset(var.s3_for_gitlab)
-  bucket = format("%s-%s-gitlab-%s-s3", var.prefix, var.vpc_name, each.key)
+  bucket = format("%s.%s.gitlab.%s.s3", var.prefix, var.vpc_name, each.key)
   acl = "private"
-  tags = merge(var.tags, tomap({Name = format("%s-%s-gitlab-%s-s3", var.prefix, var.vpc_name, each.key)}))
+  tags = merge(var.tags, tomap({Name = format("%s.%s.gitlab.%s.s3", var.prefix, var.vpc_name, each.key)}))
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
   for_each = toset(var.s3_for_gitlab)
-  bucket = format("%s-%s-gitlab-%s-s3", var.prefix, var.vpc_name, each.key)
+  bucket = format("%s.%s.gitlab.%s.s3", var.prefix, var.vpc_name, each.key)
   block_public_acls = true
   block_public_policy = true
   ignore_public_acls = true
