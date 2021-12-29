21c0a3a05d75c0d77a786dd48df6825ab2ce53a6
sgbyeon <sg.byeon1@gmail.com>
2021-12-29 15:25:58 +0900

    [dev] updates

--- gitlab.tf
+++ gitlab.tf
@@ -40,13 +40,13 @@ resource "aws_network_interface" "this" {
 resource "null_resource" "gitlab-install" {
   provisioner "file" {
     source = "./setup-gitlab.sh"
-    destination = "~/setup-gitlab.sh"
+    destination = "setup-gitlab.sh"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "chmod 700 ~/setup-gitlab.sh",
-      "sudo ~/setup-gitlab.sh"
+      "chmod 700 setup-gitlab.sh",
+      "sudo setup-gitlab.sh"
     ]
   }
 
